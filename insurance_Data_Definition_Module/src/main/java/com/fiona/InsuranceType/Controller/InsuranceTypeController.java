d19e2b8a52d3f70b3153719fc88045226446cab0
fMurugi <fionamurugi99@gmail.com>
2023-05-30 13:03:19 +0300

    made todo changes

--- insurance_Data_Definition_Module/src/main/java/com/fiona/InsuranceType/Controller/InsuranceTypeController.java
+++ insurance_Data_Definition_Module/src/main/java/com/fiona/InsuranceType/Controller/InsuranceTypeController.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.fiona.utilities.ApiResponseBuilder.buildResponseEntity;
@@ -20,43 +19,46 @@ import static com.fiona.utilities.ApiResponseBuilder.buildResponseEntity;
 //        ! todo  ADD THE VERSION OF THE ENDPOINT BEING USED. I.E (api/v1/insurance_type)
 //        ! todo  USE THE FOLLOWING CONVENTIONS TO CREATE YOUR ENDPOINT URLS  (create_????, fetch_all_???, fetch_single_????, update_single_?? delete_single_???)
 
-@RequestMapping("/api/insuranceType")
+@RequestMapping("/api/v1/insurance_type")
 public class InsuranceTypeController {
-    @Autowired
     private InsuranceTypeService insuranceTypeService;
 
     //        ! todo  UNUSED INJECTION.
-    @Autowired
     private InsuranceTypeRepository insuranceTypeRepository;
 
     //        ! todo  UNUSED CONSTANT.
     public static final String SUCCESS = "Success";
+
+    public InsuranceTypeController(InsuranceTypeService insuranceTypeService, InsuranceTypeRepository insuranceTypeRepository) {
+        this.insuranceTypeService = insuranceTypeService;
+        this.insuranceTypeRepository = insuranceTypeRepository;
+    }
 
 
     /**
      * @param insuranceTypeDTO
      * @return
      */
-    @PostMapping("/create")
+    @PostMapping("/create/insurance_type")
     public ResponseEntity<APIResponse> createInsuranceType(@RequestBody @Valid InsuranceTypeDTO insuranceTypeDTO){
         InsuranceTypeDTO insuranceTypeDTORes  = insuranceTypeService.createInsuranceType(insuranceTypeDTO);
         return buildResponseEntity(HttpStatus.CREATED,insuranceTypeDTORes,"created","/api/insuranceType/create");
     }
 
-    @GetMapping("/all")
+    @GetMapping("/fetch/all_insurance_types")
     public ResponseEntity<APIResponse> getAllInsuranceTypes(){
        List<InsuranceTypeDTO> insuranceTypeDTOList = insuranceTypeService.getAllInsuranceTypes();
       return buildResponseEntity(HttpStatus.OK,insuranceTypeDTOList,"returned all","/api/insuranceType/all");
     }
 
-    @PostMapping("/update")
+    @PostMapping("/update/single_insurance_types")
 //! todo  the implementation of the vaiable payload is commendable.
     public ResponseEntity<APIResponse> updateInsuranceType(@RequestBody InsuranceTypeDTO payload){
         List<InsuranceTypeDTO> insuranceTypeDTOList =  insuranceTypeService.updateInsuranceType(payload);
         return  buildResponseEntity(HttpStatus.CREATED,insuranceTypeDTOList,"update successful","/api/insuranceType/update");
     }
 
-    @PostMapping("/delete")
+    @PostMapping("/delete/single_insurance_type")
     public ResponseEntity<APIResponse> deleteInsuranceType(@RequestBody InsuranceTypeDTO payload){
         InsuranceTypeDTO insuranceTypeDTO = insuranceTypeService.deleteInsuranceType(payload);
         return  buildResponseEntity(HttpStatus.OK,insuranceTypeDTO,"deleted successfuly","api/insuranceType/delete");
