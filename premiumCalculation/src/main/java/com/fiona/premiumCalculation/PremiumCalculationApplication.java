c8900e7eb740ace08ad8453e9616ee0ae88531a3
fMurugi <fionamurugi99@gmail.com>
2023-06-05 14:09:40 +0300

    Initial commit

---
+++ premiumCalculation/src/main/java/com/fiona/premiumCalculation/PremiumCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.fiona.premiumCalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremiumCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremiumCalculationApplication.class, args);
+	}
+
+}
---
+++ premiumCalculation/src/test/java/com/fiona/premiumCalculation/PremiumCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiona.premiumCalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremiumCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
