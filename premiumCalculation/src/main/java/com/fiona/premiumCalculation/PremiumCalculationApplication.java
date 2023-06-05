490da19f78aced742729485ac9381ef475b99253
fMurugi <fionamurugi99@gmail.com>
2023-06-05 15:24:18 +0300

    changes on pom.xml

--- premiumCalculation/src/main/java/com/fiona/premiumCalculation/PremiumCalculationApplication.java
+++ premiumCalculation/src/main/java/com/fiona/premiumCalculation/PremiumCalculationApplication.java
@@ -2,8 +2,10 @@ package com.fiona.premiumCalculation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PremiumCalculationApplication {
 
 	public static void main(String[] args) {
