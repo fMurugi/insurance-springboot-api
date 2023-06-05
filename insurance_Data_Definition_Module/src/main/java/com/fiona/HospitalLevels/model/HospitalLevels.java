d5a90290c0d64510d11b1e1cffc77992678f73c5
fMurugi <fionamurugi99@gmail.com>
2023-06-05 09:41:13 +0300

    Added endpoints for hospital levels

--- insurance_Data_Definition_Module/src/main/java/com/fiona/HospitalLevels/model/HospitalLevels.java
+++ insurance_Data_Definition_Module/src/main/java/com/fiona/HospitalLevels/model/HospitalLevels.java
@@ -2,23 +2,27 @@ package com.fiona.HospitalLevels.model;
 //        ! todo  KINDLY ADD THE FILE, TO A FOLDER STSTING EHAT IT IS SUPPOSED TO DO, i.e repository,model,service e.t.c -dont understand this
 import com.fiona.ServiceProviders.Model.ServiceProviderModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Data
 @Table(name="data_hospital_levels")
+@AllArgsConstructor
+@NoArgsConstructor
 public class HospitalLevels {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name="levelId")
-    @NonNull
     private UUID levelId;
     @Column(name="levelName")
     private String name;
-    @OneToMany(mappedBy = "hospitalLevel")
-    private ServiceProviderModel serviceProvider;
+    @OneToMany(mappedBy = "levelId",cascade = CascadeType.ALL,orphanRemoval=true)
+    private List<ServiceProviderModel> serviceProvider;
 
 }
