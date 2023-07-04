18ace634d95c71a7373dea2fe5728f0ea2161150
fMurugi <fionamurugi99@gmail.com>
2023-07-04 12:03:49 +0300

    Added logback xml file

--- premiumCalculation/src/main/java/com/fiona/premiumCalculation/Config/SentryConfig.java
+++ premiumCalculation/src/main/java/com/fiona/premiumCalculation/Config/SentryConfig.java
@@ -1,13 +1,14 @@
-//package com.fiona.premiumCalculation.Config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.HandlerExceptionResolver;
-//
-//@Configuration
-//public class SentryConfig {
+package com.fiona.premiumCalculation.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Configuration
+public class SentryConfig {
 //    @Bean
 //    public HandlerExceptionResolver sentryExceptionResolver() {
 //        return new io.sentry.spring.SentryExceptionResolver();
 //    }
-//}
+    //adding user info
+}
